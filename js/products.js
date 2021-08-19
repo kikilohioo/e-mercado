86c23ad5f14d255e74ada794e7bfaa34ff7ca1da
Caiqui Viera <cvcommunitymanager@gmail.com>
2021-08-19 10:39:53 -0300

    recreate repository

---
+++ js/products.js
@@ -0,0 +1,30 @@
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function (e) {
+    let main = document.getElementById("main");
+    main.innerHTML = "";
+    fetch(PRODUCTS_URL)
+        .then(respuesta => respuesta.json())
+
+        .then(datos => {
+            for(let producto of datos){
+                main.innerHTML += 
+                `<a href="product-info.html" class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="`+ producto.imgSrc + `" alt="`+ producto.name +`" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">`+ producto.name +`</h4>
+                                <small class="text-muted">`+ producto.currency + ` ` + producto.cost + `</small>
+                            </div>
+                            <p class="mb-1">`+ producto.description +`</p>
+                            <p class="mb-1">Vendidos `+ producto.soldCount +`</p>
+                        </div>
+                    </div>
+                </a>`;
+            }
+        }).catch(error => alert(error))
+});
