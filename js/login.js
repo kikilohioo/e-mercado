4488aaaa2c7ce3ef8afe861c4171b3d24275c8ed
Caiqui Viera <cvcommunitymanager@gmail.com>
2021-08-26 08:46:26 -0300

    updates

--- js/login.js
+++ js/login.js
@@ -4,6 +4,20 @@
 function onSignIn(){
     window.location.href = "inicio.html";
 }
+function getUserName(){
+    let email = document.getElementById("user").value;
+    let mistorage = window.localStorage;
+    fetch("../users.json")
+    .then(respuesta => respuesta.json())
+    .then(datos => {
+        for(let user of datos){
+            if(email == user.name){
+                mistorage.setItem('username', user.name);
+            }
+        }
+    }).
+    catch(error => alert(error));
+}
 document.addEventListener("DOMContentLoaded", function(e){
     
 });
