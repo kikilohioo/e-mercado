f446520e7bcfa5a930303e7a7d8470f6bd6ddab8
Caiqui Viera <cvcommunitymanager@gmail.com>
2021-08-27 16:35:03 -0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,8 +1,37 @@
-function 
+let results = document.getElementById("results");
+let searchbox = document.getElementById("searchbox").value;
+
+function searchFromResultList(articulo){
+    searchbox = articulo;
+    results.style.display = "none";
+    let productgrid = document.getElementById("productgrid");
+
+    fetch(PRODUCTS_URL)
+        .then(respuesta => respuesta.json())
+        .then(productos => {
+            for(let producto of productos){
+                productgrid.innerHTML += 
+                `<a href="product-info.html" class="list-group-item list-group-item-action">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="`+ producto.imgSrc + `" alt="`+ producto.name +`" class="img-thumbnail">
+                        </div>
+                        <div class="col">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">`+ producto.name +`</h4>
+                                <small class="text-muted">`+ producto.currency + ` ` + producto.cost + `</small>
+                            </div>
+                            <p class="mb-1">`+ producto.description +`</p>
+                            <p class="mb-1">Vendidos `+ producto.soldCount +`</p>
+                        </div>
+                    </div>
+                </a>`;
+            }
+        }).catch(error => alert(error))
+}
 
 document.getElementById("searchbox").addEventListener("keyup", function(e){
-    let results = document.getElementById("results");
-    let searchbox = document.getElementById("searchbox").value.toLowerCase();
+    searchbox.toLowerCase();
     results.innerHTML ="";
     if(searchbox != ""){
         fetch(PRODUCTS_URL)
@@ -12,7 +41,7 @@ document.getElementById("searchbox").addEventListener("keyup", function(e){
                 for(let producto of productos){
                     let nombre = producto.name.toLowerCase();
                     if(nombre.indexOf(searchbox) != -1){
-                        results.innerHTML += `<div class="list-group-item list-group-item-action" onclick="">`+ producto.name +`</div>`;
+                        results.innerHTML += `<div class="list-group-item list-group-item-action" onclick="searchFromResultList("`+ producto.name +`")">`+ producto.name +`</div>`;
                         results.style.display = "block";
                     }
                 }
