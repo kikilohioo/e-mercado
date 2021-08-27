250c2f9b517991ee0d8320ac53c6a2e9ac226fb0
Caiqui Viera <cvcommunitymanager@gmail.com>
2021-08-27 01:21:37 -0300

    search attempt

--- js/search.js
+++ js/search.js
@@ -1,6 +1,6 @@
 document.getElementById("searchbox").addEventListener("keyup", function(e){
     let results = document.getElementById("results");
-    let searchbox = document.getElementById("searchbox");
+    let searchbox = document.getElementById("searchbox").value.toLowerCase();
 
     if(searchbox.value != ""){
         results.style.display = "block";
@@ -10,7 +10,12 @@ document.getElementById("searchbox").addEventListener("keyup", function(e){
 
     fetch(PRODUCTS_URL)
     .then(respuesta => respuesta.json())
-    .then(datos => {
-        
+    .then(productos => {
+        for(let producto of productos){
+            let nombre = producto.name.toLowerCase();
+            if(nombre.indexOf(searchbox) != -1){
+                results.innerHTML += `<a href="#" class="list-group-item list-group-item-action">`+ producto.name +`</a>`;
+            }
+        }
     }).catch(error => alert(error))
 })
