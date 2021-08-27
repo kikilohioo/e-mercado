81525107cff9e6467ebe8a74d873c186ed2fdcea
Caiqui Viera <cvcommunitymanager@gmail.com>
2021-08-27 17:43:25 -0300

    sa

--- js/login.js
+++ js/login.js
@@ -2,18 +2,22 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 let USERS_URL = "https://kikilohioo.github.io/e-mercado/users.json";
-function onSignIn(){
+let mistorage = window.localStorage;
+
+function onSignIn(googleUser){
+    var profile = googleUser.getBasicProfile();
+    mistorage.setItem('username', profile.getName());
     window.location.href = "inicio.html";
 }
 function getUserName(){
     let email = document.getElementById("user").value;
-    let mistorage = window.localStorage;
     fetch(USERS_URL)
         .then(respuesta => respuesta.json())
         .then(users => {
             for(let user of users){
                 if(email == user.email){
                     mistorage.setItem('username', user.name);
+                    window.location.href = "inicio.html";
                 }
             }
         }).catch(error => alert(error))
--- js/search.js
+++ js/search.js
@@ -1,16 +1,27 @@
+function searchFromResultList(nombreart){
+    let results = document.getElementById("results"); 
+    results.style.display = "none";
+    let searchbox = document.getElementById("searchbox").value;
+    searchbox = nombreart;
+}
 document.getElementById("searchbox").addEventListener("keyup", function(e){
     let results = document.getElementById("results");
-    let searchbox = document.getElementById("searchbox");
-
-    if(searchbox.value != ""){
-        results.style.display = "block";
+    let searchbox = document.getElementById("searchbox").value.toLowerCase();
+    results.innerHTML ="";
+    if(searchbox != ""){
+        fetch(PRODUCTS_URL)
+        .then(respuesta => respuesta.json())
+        .then(productos => {
+                for(let producto of productos){
+                    let nombre = producto.name.toLowerCase();
+                    if(nombre.indexOf(searchbox) != -1){
+                        results.innerHTML += `<div class="list-group-item list-group-item-action" onclick="searchFromResultList("`+ producto.name +`")">`+ producto.name +`</div>`;
+                        results.style.display = "block";
+                    }
+                }
+        }).catch(error => alert(error))
     }else{
         results.style.display = "none";
     }
-
-    fetch(PRODUCTS_URL)
-    .then(respuesta => respuesta.json())
-    .then(datos => {
-        
-    }).catch(error => alert(error))
 })
+
