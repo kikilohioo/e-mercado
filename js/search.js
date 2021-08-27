863f211e02b16a2c2b2e1e025698267b85e3d7bb
Caiqui Viera <cvcommunitymanager@gmail.com>
2021-08-27 16:15:08 -0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -3,23 +3,20 @@ document.getElementById("searchbox").addEventListener("keyup", function(e){
     let searchbox = document.getElementById("searchbox").value.toLowerCase();
     results.innerHTML ="";
     if(searchbox.value != ""){
-        results.style.display = "block";
+        fetch(PRODUCTS_URL)
+        .then(respuesta => respuesta.json())
+        .then(productos => {
+            
+                for(let producto of productos){
+                    let nombre = producto.name.toLowerCase();
+                    if(nombre.indexOf(searchbox) != -1){
+                        results.innerHTML += `<div class="list-group-item list-group-item">`+ producto.name +`</div>`;
+                        results.style.display = "block";
+                    }
+                }
+            
+        }).catch(error => alert(error))
     }else{
         results.style.display = "none";
     }
-
-    fetch(PRODUCTS_URL)
-    .then(respuesta => respuesta.json())
-    .then(productos => {
-        if(searchbox.value != ""){
-            for(let producto of productos){
-                let nombre = producto.name.toLowerCase();
-                if(nombre.indexOf(searchbox) != -1){
-                    results.innerHTML += `<a href="#" class="list-group-item list-group-item-action">`+ producto.name +`</a>`;
-                }
-            }
-        }else{
-            results.style.display = "none";
-        }
-    }).catch(error => alert(error))
 })
