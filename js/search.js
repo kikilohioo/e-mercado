0a250240252c1d6694253afee4cd82b3a7bae911
Caiqui Viera <cvcommunitymanager@gmail.com>
2021-08-27 17:10:23 -0300

    e

--- js/search.js
+++ js/search.js
@@ -1,7 +1,7 @@
-let results = document.getElementById("results");
-let searchbox = document.getElementById("searchbox").value.toLowerCase();
 
 document.getElementById("searchbox").addEventListener("keyup", function(e){
+    let results = document.getElementById("results");
+    let searchbox = document.getElementById("searchbox").value.toLowerCase();
     results.innerHTML ="";
     if(searchbox != ""){
         fetch(PRODUCTS_URL)
@@ -10,7 +10,7 @@ document.getElementById("searchbox").addEventListener("keyup", function(e){
                 for(let producto of productos){
                     let nombre = producto.name.toLowerCase();
                     if(nombre.indexOf(searchbox) != -1){
-                        results.innerHTML += `<div class="list-group-item list-group-item-action" onclick="">`+ producto.name +`</div>`;
+                        results.innerHTML += `<div class="list-group-item list-group-item-action" onclick="searchFromResultList(`+ producto.name +`)">`+ producto.name +`</div>`;
                         results.style.display = "block";
                     }
                 }
@@ -19,3 +19,34 @@ document.getElementById("searchbox").addEventListener("keyup", function(e){
         results.style.display = "none";
     }
 })
+
+function searchFromResultList(nombreArt){
+    let results = document.getElementById("results"); 
+    results.style.display = "none";
+    let searchbox = document.getElementById("searchbox").value;
+    searchbox = nombreArt;
+
+ /*   fetch(PRODUCTS_URL)
+        .then(respuesta => respuesta.json())
+        .then(producto => { 
+            productgrid.innerHTML += 
+            `<a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="`+ producto.imgSrc + `" alt="`+ producto.name +`" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ producto.name +`</h4>
+                            <small class="text-muted">`+ producto.currency + ` ` + producto.cost + `</small>
+                        </div>
+                        <p class="mb-1">`+ producto.description +`</p>
+                        <p class="mb-1">Vendidos `+ producto.soldCount +`</p>
+                    </div>
+                </div>
+            </a>`;
+
+     })
+     .catch(error => alert(error)) */ 
+
+}
