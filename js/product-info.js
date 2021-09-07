de8c56b41457d5bf25cd17a3a9737a0e776222db
Caiqui Viera <cvcommunitymanager@gmail.com>
2021-09-07 14:29:54 -0300

    products images

--- js/product-info.js
+++ js/product-info.js
@@ -18,11 +18,14 @@ document.addEventListener("DOMContentLoaded", function(e){
                     `<div class="w3-center w3-container w3-section w3-large w3-text-white w3-display-bottommiddle" style="width:100%">
                         <div class="w3-left w3-hover-text-khaki" onclick="plusDivs(-1)">&#10094;</div>
                         <div class="w3-right w3-hover-text-khaki" onclick="plusDivs(1)">&#10095;</div>`;
-                    for(var i = 1; i < info.images.lenght; i++){
+                    var i = 0;
+                    for(let image of info.images){
+                        i++;
                         slider += `<span class="w3-badge demo w3-border w3-transparent w3-hover-white" onclick="currentDiv(`+ i +`)"></span>`;
                     }
-                    slider += `</div></div><script src="js/slider.js"></script>`;
+                    slider += `</div></div>`;
                     main.innerHTML += slider;
+                    showDivs(slideIndex);
                 }
             }
         })
