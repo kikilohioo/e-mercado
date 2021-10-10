3cf798920bbb84f3eac17210f9b20b366155b83a
Caiqui Viera <cvcommunitymanager@gmail.com>
2021-10-09 23:27:49 -0300

    cart functions

--- js/cart.js
+++ js/cart.js
@@ -2,11 +2,23 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-    
-    fetch().
+    let listItemsContainer = document.getElementById("productItems");
+    fetch(CART_INFO_URL).
         then(respuesta => respuesta.json()).
             then(items => {
-                
+                let articulos = items.articles;
+                for(let articulo of articulos){
+                    listItemsContainer.innerHTML += 
+                    `<tr>
+                        <td class="d-none d-lg-block">
+                            <img src="`+ articulo.src +`" width="50" alt="prod1">
+                        </td>
+                        <td>`+ articulo.name +`</td>
+                        <td class="d-none d-lg-block">`+ articulo.currency + articulo.unitCost +`</td>
+                        <td><input type="number" name="" id="" value="`+ articulo.count +`" style="width: 50px;"></td>
+                        <td>`+ articulo.unitCost +`</td>
+                    </tr>`;
+                }
             }).
-    catch(error => aleert(error))
+    catch(error => alert(error))
 });
